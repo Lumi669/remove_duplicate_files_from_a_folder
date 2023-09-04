98fe16baddd3fd8086624fafb7c79732894cb2df
cmjiwa <wangjinghuan@yahoo.com>
2023-09-04 20:31:50 +0300

    now remove duplicated files for both .jpeg and .mov type

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,32 +1,24 @@
 import os
 import hashlib
 
-def calculate_hash(file_path):
-    # Calculate the MD5 hash of the file
-    hash_md5 = hashlib.md5()
-    with open(file_path, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
-
 def remove_duplicate_files(folder_path):
-    # Create a dictionary to store file hashes and paths
-    file_hashes = {}
-    
+    # Create a dictionary to store file sizes and paths
+    file_sizes = {}
+
     # Iterate through files in the folder
     for root, _, files in os.walk(folder_path):
         for file in files:
             file_path = os.path.join(root, file)
             
-            # Calculate the hash of the file
-            file_hash = calculate_hash(file_path)
+            # Get the size of the file
+            file_size = os.path.getsize(file_path)
             
-            # If a file with the same hash already exists, remove the duplicate
-            if file_hash in file_hashes:
+            # If a file with the same size already exists, remove the duplicate
+            if file_size in file_sizes:
                 print(f"Removing duplicate: {file_path}")
                 os.remove(file_path)
             else:
-                file_hashes[file_hash] = file_path
+                file_sizes[file_size] = file_path
 
 if __name__ == "__main__":
     folder_path = "/Users/jinghuan/Desktop/170723_from_mobile"  # Replace with the path to your folder
